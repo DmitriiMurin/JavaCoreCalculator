e028b2f43e37442d2715ceccc674a9530bb2f9ad
DmitriiMurin <murin.dima@gmail.com>
2023-03-20 13:37:21 +0300

    Initial commit

---
+++ Calculator.java
@@ -0,0 +1,21 @@
+package JavaCore.HomeWork.Calculator;
+
+import java.util.function.*;
+
+public class Calculator {
+    static Supplier<Calculator> instance = Calculator::new;
+    BinaryOperator<Integer> plus = (x, y) -> x + y;
+    BinaryOperator<Integer> minus = (x, y) -> x - y;
+    BinaryOperator<Integer> multiply = (x, y) -> x * y;
+    BinaryOperator<Integer> divide = (x, y) -> {
+        if (y != 0) return x / y;
+        else {
+            throw new ArithmeticException("Divide by ZERO");
+        }
+    };
+    UnaryOperator<Integer> pow = x -> x * x;
+    UnaryOperator<Integer> abs = x -> x > 0 ? x : x * -1;
+    Predicate<Integer> isPositive = x -> x > 0;
+    Consumer<Integer> println = System.out::println;
+}
+
---
+++ Main.java
@@ -0,0 +1,17 @@
+package JavaCore.HomeWork.Calculator;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Calculator calc = Calculator.instance.get();
+
+        int a = calc.plus.apply(1, 2);
+        int b = calc.minus.apply(1, 1);
+        int c = calc.divide.apply(a, b); // Делим на ноль, в связи с этим получаем ошибку.
+
+        calc.println.accept(c);
+    }
+
+
+}
